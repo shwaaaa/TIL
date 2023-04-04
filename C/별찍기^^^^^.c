39471da751fe5e3329461c447fcc34108c66f4cd
shwaaaa <shwaa1026@naver.com>
2023-04-04 15:54:15 +0900

    다이아몬드 별찍기

--- C/별찍기^^^^^.c
+++ C/별찍기^^^^^.c
@@ -11,4 +11,57 @@ for (x = 5; x > 0; x--) {
 return 0;
 }
 
+#include <stdio.h>
+int main() {
+    int x;
+    scanf("%d", &x);
+    for (int i = 1; i <=x; i++) {
+        for (int j = x; j >= i; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+	return 0;
+}
+
+#include <stdio.h>
+int main() {
+    int x;
+    scanf("%d", &x);
+    for (int i = 1; i <=x; i++) {
+        for (int j = 1; j <= i-1; j++) {
+            printf(" ");
+        }
+        for (int j = x; j >= i; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+	return 0;
+}
+
 //다이아몬드 찍기
+  int input;
+    scanf("%d", &input);
+    for (int i = 0; i < input; i++) {
+        for (int j = 1; j <input- i; j++) {
+            printf(" ");
+        }
+
+        for (int j = 0; j < 2 * i + 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 1; i < input; i++) {
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+
+        for (int j = 0; j < 2*input-(2*i+1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
