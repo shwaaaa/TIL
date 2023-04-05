c76910935027c1ab193514c42e6e3530d26e1f6e
shwaaaa <shwaa1026@naver.com>
2023-04-05 20:05:29 +0900

    별찍기

--- C/별찍기^^^^^.c
+++ C/별찍기^^^^^.c
@@ -1,28 +1,50 @@
-//역삼각형 찍기
+//직각삼각형 찍기
 
+//*
+//**
+//***
+//****
+//*****
 #include <stdio.h>
-int main(){
-int x, y;
-for (x = 5; x > 0; x--) {
-	for (y = 1; y < x+1; y++)
-		printf("*");
-	printf("\n");
-}
-return 0;
-}
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+		return 0;
+	}
+
+
+//역직각삼각형 찍기
+//*****
+//****
+//***
+//**
+//*
 
 #include <stdio.h>
 int main() {
-    int x;
-    scanf("%d", &x);
-    for (int i = 1; i <=x; i++) {
-        for (int j = x; j >= i; j--) {
-            printf("*");
-        }
-        printf("\n");
-    }
-	return 0;
-}
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = n; j > i ; j--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+		return 0;
+	}
+
+
+//*****
+// ****
+//  ***
+//   **
+//    *
 
 #include <stdio.h>
 int main() {
@@ -40,28 +62,83 @@ int main() {
 	return 0;
 }
 
+
+//삼각형 찍기
+//   *
+//  ***
+// *****
+
+#include <stdio.h>
+int main(){
+int input;
+scanf("%d", &input);
+for (int i = 0; i < input; i++) {
+    for (int j = 1; j < input - i; j++) {
+        printf(" ");
+    }
+
+    for (int j = 0; j < 2 * i + 1; j++) {
+        printf("*");
+    }
+    printf("\n");
+}
+return 0;
+}
+
+
+//역삼각형 찍기
+// *****
+//  ***
+//   *
+
+#include <stdio.h>
+int main(){
+    int input;
+    scanf("%d",&input);
+for (int i = 1; i < input; i++) {
+    for (int j = 0; j < i; j++) {
+        printf(" ");
+    }
+
+    for (int j = 0; j < 2 * input - (2 * i + 1); j++) {
+        printf("*");
+    }
+    printf("\n");
+}
+return 0;
+}
+
+
 //다이아몬드 찍기
-  int input;
-    scanf("%d", &input);
-    for (int i = 0; i < input; i++) {
-        for (int j = 1; j <input- i; j++) {
-            printf(" ");
-        }
+//  *
+// ***
+//*****
+// ***
+//  *
 
-        for (int j = 0; j < 2 * i + 1; j++) {
-            printf("*");
-        }
-        printf("\n");
+ #include <stdio.h>
+int main(){
+int input;
+scanf("%d", &input);
+for (int i = 0; i < input; i++) {
+    for (int j = 1; j < input - i; j++) {
+        printf(" ");
     }
-    for (int i = 1; i < input; i++) {
-        for (int j = 0; j < i; j++) {
-            printf(" ");
-        }
 
-        for (int j = 0; j < 2*input-(2*i+1); j++) {
-            printf("*");
-        }
-        printf("\n");
+    for (int j = 0; j < 2 * i + 1; j++) {
+        printf("*");
     }
-    return 0;
+    printf("\n");
+}
+for (int i = 1; i < input; i++) {
+    for (int j = 0; j < i; j++) {
+        printf(" ");
+    }
+
+    for (int j = 0; j < 2 * input - (2 * i + 1); j++) {
+        printf("*");
+    }
+    printf("\n");
+}
+return 0;
 }
