e176e45534e50abc0e83ceee7ce038d35080f064
shwaaaa <shwaa1026@naver.com>
2023-04-10 20:25:32 +0900

    JSON 정리

---
+++ C/러ㅇ니.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int x;
+	for (int i = 2; i<=9; i++) {
+		for(x=1; x<10; x++){
+            if(i==2)
+				printf("%d * %d = %d₩n",i,x,i*x);
+        }
+	}
+return 0;
+}
