149b0ae7fbc736cc61095068657c131ec8088942
shwaaaa <shwaa1026@naver.com>
2023-04-17 20:08:50 +0900

    방과후후c

--- C/방과후c.c
+++ C/방과후c.c
@@ -28,20 +28,36 @@
 // }
 
 
-#include <stdio.h>
-int Power(int a);
-int main(){
-    int x;
-    int result;
-    scanf("%d",&x);
-    result = Power(x);
-    printf("%d",result);
-    return 0;
-}
-int Power(int a){
-    int sum=1;
-    for(int i=1; i<=a; i++){
-        sum=sum*2;
-    }
-    return sum;
-}
+// #include <stdio.h>
+// int Power(int a);
+// int main(){
+//     int x;
+//     int result;
+//     scanf("%d",&x);
+//     result = Power(x);
+//     printf("%d",result);
+//     return 0;
+// }
+// int Power(int a){
+//     int sum=1;
+//     for(int i=1; i<=a; i++){
+//         sum=sum*2;
+//     }
+//     return sum;
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     int a[7]={5,10,4,8,90,2,8};
+//     for(int i=0; i<7; i++){
+//     printf("%d\n",a[i]);}
+//     int a2[5]={0,1,2,3,4};
+//     for(int i=0; i<5; i++){
+//         printf("%d\n",a2[i]);
+//     }
+//     int a3[3]={0,1,2};
+//     for(int i=0; i<3; i++){
+//         printf("%d\n",a3[i]);
+//     }
+// }
