97e7f04df4fb23c822e45335c6d655ff85c3fd14
shwaaaa <shwaa1026@naver.com>
2023-05-22 16:44:34 +0900

    c 포인터

--- C/포인터.c
+++ C/포인터.c
@@ -42,14 +42,46 @@
 
 
 //10-2
-#include<stdio.h>
-void get_int(int* px, int* py);
-int main(void){
-    int x, y;
-    get_int(&x, &y);
-    printf("정수의 합은 %d\n", x+y);
-    return 0;
-}
-void get_int(int* px, int* py){
-    scanf("%d %d", px, py);
+// #include<stdio.h>
+// void get_int(int* px, int* py);
+// int main(void){
+//     int x, y;
+//     get_int(&x, &y);
+//     printf("정수의 합은 %d\n", x+y);
+//     return 0;
+// }
+// void get_int(int* px, int* py){
+//     scanf("%d %d", px, py);
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//     int a[] = {1, 2, 3, 4, 5};
+//     printf("%d\n");
+
+//     for(int i=0; i<5; i++){
+//         printf("값 = %d,주소 = %d\n", a[i], &a[i]);
+//         printf("값 = %d, 주소 = %d\n",*(a+i),a+i);
+//     }
+// }
+
+#include <stdio.h>
+int main() {
+    //포인터에 배열을 대입했을 경우
+    int a[] = {10, 20, 30, 40, 50};
+    int* p = a;
+
+    //a++ 절대 X
+    //p++ 가능
+    //포인터를 사용해서 배열 데이터 출력하기
+    printf("주소 : %d\n", p);
+    printf("데이터 : %d\n", *p);
+
+    //반복문 사용해서 배열 전체 데이터 출력하기
+    for (int i=0; i<5; i++){
+        printf("주소 : %d, 데이터 : %d\n", p+i, *(p+i));
+        printf("주소 : %d, 데이터 : %d\n", &p[i], p[i]);
+        printf("\n");
+    }
 }
